a24792a8517bb9c1e767d14e9967891a68eed984
pakjivega <pakjivega@gmail.com>
2019-12-03 23:12:17 +0100

    quickStart Archetype

---
+++ src/main/java/com/jivega/patterns/fizzbuzz/App.java
@@ -0,0 +1,13 @@
+package com.jivega.patterns.fizzbuzz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
