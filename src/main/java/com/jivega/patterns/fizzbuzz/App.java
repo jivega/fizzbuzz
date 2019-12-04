f9d9f3a6f5a4a15d8907455db1b06eb0ef32622b
pakjivega <pakjivega@gmail.com>
2019-12-04 01:45:47 +0100

    Stage2

--- src/main/java/com/jivega/patterns/fizzbuzz/App.java
+++ src/main/java/com/jivega/patterns/fizzbuzz/App.java
@@ -4,14 +4,21 @@ import java.util.stream.IntStream;
 
 public class App {
     public static void main(String[] args) {
-        IntStream.range(1,101).forEachOrdered( i-> System.out.println(getMessage(i)));
+        IntStream.range(1, 101).forEachOrdered(i -> System.out.println(getMessage(i)));
     }
-    public static String getMessage( int i ) {
-        String result ;
-        if ( i % 15 == 0 ) { result = "BizzBuzz";}
-        else if ( i % 5 == 0 ) { result = "Buzz";}
-        else if ( i % 3 == 0 ) { result = "Bizz";}
-        else { result = Integer.valueOf(i).toString();}
-        return result;
+
+    public static String getMessage(int i) {
+        String number = Integer.valueOf(i).toString();
+        StringBuilder result = new StringBuilder();
+        if ((i % 3 == 0) || (number.contains("3"))) {
+            result.append("Bizz");
+        }
+        if ((i % 5 == 0) || (number.contains("5"))) {
+            result.append("Buzz");
+        }
+        if (result.length() == 0) {
+            result.append(number);
+        }
+        return result.toString();
     }
 }
--- src/test/java/com/jivega/patterns/fizzbuzz/AppTest.java
+++ src/test/java/com/jivega/patterns/fizzbuzz/AppTest.java
@@ -13,6 +13,10 @@ public class AppTest {
         int j = 16;
         int k = 20;
         int l = 21;
+        int m = 31;
+        int n = 51;
+        int o = 53;
+
         String messageBizzBuzz = "BizzBuzz";
         String messageBizz = "Bizz";
         String messageBuzz = "Buzz";
@@ -20,5 +24,9 @@ public class AppTest {
         Assert.assertTrue(messageBizz.equals(App.getMessage(l)));
         Assert.assertTrue(messageBuzz.equals(App.getMessage(k)));
         Assert.assertTrue(Integer.valueOf(j).toString().equals(App.getMessage(j)));
+        Assert.assertTrue(messageBizz.equals(App.getMessage(m)));
+        Assert.assertTrue(messageBizzBuzz.equals(App.getMessage(n)));
+        Assert.assertTrue(messageBizzBuzz.equals(App.getMessage(o)));
+
     }
 }
